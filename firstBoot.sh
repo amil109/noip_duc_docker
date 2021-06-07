40c6af385b1feb4258c011ddcb8cf751d6abc121
Andrew Mills <amil109@users.noreply.github.com>
2021-06-06 19:21:39 -0500

    Update firstBoot.sh

--- firstBoot.sh
+++ firstBoot.sh
@@ -1,1 +1,8 @@
-
+CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
+if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
+    touch $CONTAINER_ALREADY_STARTED
+    echo "-- First container startup --"
+    cd /usr/local/src/noip* && make && make install
+else
+    echo "-- Not first container startup --"
+fi
